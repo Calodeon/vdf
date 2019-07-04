b7ddc11571a67fde50e48467abeb309452483558
Benjamin Wesolowski <benjamin.wesolowski@cwi.nl>
2019-07-04 12:09:37 +0200

    import estimation script

--- estimation.py
+++ estimation.py
@@ -1,10 +1,10 @@
 
-
+import math;
 
 # returns the time to compute the proof using Argon Design's algorithm, with 
 # parameters gamma and kappa as in their report
 def cost_of_proof(T, gamma,kappa, parameters):
-    return (parameters['mult_square_ratio']*(T / kappa + gamma * (2**kappa))).ceil();
+    return math.ceil(parameters['mult_square_ratio']*(T / kappa + gamma * (2**kappa)));
 
 
 # finds the optimal parameters gamma and kappa for time parameter T, to minimize 
@@ -14,11 +14,11 @@ def optimize(T,parameters):
     bestKappa = 0
     bestCost = 0
     
-    for kappa in range(1,(log(T)/log(2)).n().ceil()):
+    for kappa in range(1,math.ceil(math.log(T)/math.log(2))):
         if parameters['memory_bound'] > 0:
             # smallest gamma such that:
             # T/(gamma*kappa)*parameters['modulus_length'] < parameters['memory_bound']
-            gamma = (T/(parameters['memory_bound']*kappa)*parameters['modulus_length']).ceil();
+            gamma = math.ceil(T/(parameters['memory_bound']*kappa)*parameters['modulus_length']);
         else:
             gamma = 1;
         time_cost = cost_of_proof(T,gamma,kappa, parameters);
@@ -72,9 +72,9 @@ def optimize_iterated(T,n, parameters):
 # returns the time to compute the proof using Argon Design's algorithm, with 
 # parameters gamma and kappa as in their report
 def cost_of_proof_for_hybrid(T, N, gamma,kappa, parameters):
-    t = (T/2**N).n().ceil();
-    return (parameters['mult_square_ratio']*(t / kappa + gamma * (2**kappa) \
-        + 1.5* parameters['security_level']*2**N*t/(kappa*gamma) )).ceil();
+    t = math.ceil(T/2**N);
+    return math.ceil(parameters['mult_square_ratio']*(t / kappa + gamma * (2**kappa) \
+        + 1.5* parameters['security_level']*2**N*t/(kappa*gamma) ));
 
 # finds the optimal parameters gamma and kappa for time parameter T, to minimize 
 # the time to compute the proof given a bound on the available memory
@@ -83,11 +83,11 @@ def optimize_hybrid(T,N,parameters):
     bestKappa = 0
     bestCost = 0
     
-    for kappa in range(1,(log(T)/log(2)).n().ceil()):
+    for kappa in range(1,math.ceil(math.log(T)/math.log(2))):
         if parameters['memory_bound'] > 0:
             # smallest gamma such that:
             # T/(gamma*kappa)*parameters['modulus_length'] < parameters['memory_bound']
-            gamma = (T/(parameters['memory_bound']*kappa)*parameters['modulus_length']).ceil();
+            gamma = math.ceil(T/(parameters['memory_bound']*kappa)*parameters['modulus_length']);
         else:
             gamma = 1;
         time_cost = cost_of_proof_for_hybrid(T,N,gamma,kappa, parameters);
@@ -100,25 +100,26 @@ def optimize_hybrid(T,N,parameters):
 
 
 def test(T, parameters):
-    print "T =", T;
-    print parameters;
+    print ("T =", T);
+    print (parameters);
 
     for N in range(1,8):
-        print "Hybrid prover with", N-1, "Pietrzak rounds";
-        print "Size of proofs: ", ((N)*parameters['modulus_length'])/8, "B";
-        print "Overhead: ", (100*optimize_hybrid(T,N-1, parameters)/T).n(5), "%\n";
+        print ("Hybrid prover with", N-1, "Pietrzak rounds");
+        print ("Size of proofs: ", ((N)*parameters['modulus_length'])/8, "B");
+        print ("Overhead: ", (100*optimize_hybrid(T,N-1, parameters)/T), "%\n");
 
     for N in range(1,8):
-        print "%d-iterated prover" % N;
-        print "Size of proofs: ", (N*parameters['modulus_length'] \
-                                  + (N-1)*2*parameters['security_level'])/8, "B";
-        print "Overhead: ", (100*optimize_iterated(T,N, parameters)[1]/T).n(5), "%\n";
+        print ("%d-iterated prover" % N);
+        print ("Size of proofs: ", (N*parameters['modulus_length'] \
+                                  + (N-1)*2*parameters['security_level'])/8, "B");
+        print ("Overhead: ", (100*optimize_iterated(T,N, parameters)[1]/T), "%\n");
 
 
 parameters = {
     'mult_square_ratio': 1.2/4, # relative cost of multiplication vs squaring
     # (as an example, I say that the latency of multiplication is 1.2 times
     # the lattency of squaring, but one can do 4 multiplications in parallel)
+
     'modulus_length': 2048, # binary length of RSA modulus
     'memory_bound': 8 * 8 * 2**20, # allowed to store 8 MB of precomputed data
     'security_level': 128 # number of bits of security
